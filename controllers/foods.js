7a51852f7371374f2971aca03045188438568e18
clairecleverlamb <clairechenszeying@gmail.com>
2025-03-15 16:08:28 -0700

    wip: update create functionality

--- controllers/foods.js
+++ controllers/foods.js
@@ -3,6 +3,41 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 
-// router logic will go here - will be built later on in the lab
+router.get('/', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        res.render('foods/index.ejs', {foods: currentUser.foods});
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+});
+
+router.get('/new', async (req,res) => {
+    res.render('foods/new.ejs');
+});
+
+router.post('/', async (req, res) => {
+    try {
+       const currentUser = await User.findById(req.session.user._id);
+       currentUser.foods.push(req.body);  // new form to database 
+       await currentUser.save();
+       res.redirect(`/users/${currentUser._id}/foods`);
+    } catch(error) {
+        console.log(error);
+        res.redirect('/');
+    };
+})
+
+router.get('/:foodId', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        const food = currentUser.foods.id(req.params.foodId);
+        res.render('foods/show.ejs', {food});
+    } catch (error) {
+        console.log(error);
+        res.redirect('/')
+    }
+})
 
 module.exports = router;
